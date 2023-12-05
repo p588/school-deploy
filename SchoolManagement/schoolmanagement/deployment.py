1f033e5bd6d67c970f0de72f29cbc085903decb4
Preethi <matampreethi@gmail.com>
2023-12-05 18:22:00 +0530

    adding secret key

--- SchoolManagement/schoolmanagement/deployment.py
+++ SchoolManagement/schoolmanagement/deployment.py
@@ -2,6 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['secret']
+
 ALLOWED_HOSTS=[os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS=['http://'+os.environ['WEDSITE_HOSTNAME']]
 DEBUG=False
